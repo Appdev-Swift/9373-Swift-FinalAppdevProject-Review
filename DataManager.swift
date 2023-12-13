2ae9b48a645e2b3a80a631034ea23463f8e2b85d
SollorinJohnMichael <152826465+SollorinJohnMichael@users.noreply.github.com>
2023-12-13 19:43:36 +0800

    Added Documentation for DataManager.swift

--- DataManager.swift
+++ DataManager.swift
@@ -1,14 +1,21 @@
 //
-//  DataManager.swift
-//  Project
+//  DataManager.swift
+//  Project
 //
-//  Created by JvCervantes on 9/16/23.
+//  Created by JvCervantes on 9/16/23.
 //
 
 import Foundation
-class Manager{
+
+/// `Manager` class is responsible for managing and sharing data across the application.
+class Manager {
+    
+    /// Shared instance of the `Manager` class, following the singleton pattern.
     public static let share = Manager()
+    
+    /// An instance of the `ProductList` class, managing the list of products in the application.
     var productsList = ProductList()
-    private init(){
-    }
+    
+    /// Private initializer to enforce the singleton pattern and prevent external instantiation.
+    private init() {}
 }
